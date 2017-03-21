974e0bf33b15c9360384648181a8344efdd62fab
xiayang0705 <xiayang0705@thundersoft.com>
2017-03-21 20:06:55 +0800

    change at company

--- CPP-Algorithm/test01/test01.cpp
+++ CPP-Algorithm/test01/test01.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
+static void reading_and_writing_files()
+{
+	cout<<"enter"<<endl;
+	ifstream in("test01.cpp");
+	string result;
+	string s;
+	while(getline(in, result))
+		s += result + "\n"; /*result don't store newline charactor*/
+	cout<<s;
+}
+
 int main(int argc, char *argv[]){
 	int i;
 	cout<<"i="<<i<<endl;
+	string s1, s2;
+	string s3 = "Hello, World";
+	string s4("I am");
+	s2 = "Today";
+	s1 = s3 + " " + s4;
+	s1 += "8";
+	cout<<"s1= "<<s1<<endl;
+	reading_and_writing_files();
 	return 0;
 }
+
+
